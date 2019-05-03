149ecbc0dc5bceaa09b8fa0b51112b56932a1938
ahribori <ahribori@gmail.com>
2019-05-04 03:13:38 +0900

    누락된 @Column Decorator 추가, 불필요한 @Comment 데코레이터 제거

--- server/src/db/model/Application.ts
+++ server/src/db/model/Application.ts
@@ -18,15 +18,13 @@ class Application extends Model<Application> {
   @Column
   name!: string;
 
-  @Comment('계정 ID')
   @ForeignKey(() => Account)
+  @Column
   accountId!: number;
 
-  @Comment('어플리케이션 OWNER 계정')
   @BelongsTo(() => Account)
   account!: Account;
 
-  @Comment('어플리케이션에 가입된 계정들')
   @BelongsToMany(() => Account, () => Link)
   linkedAccounts!: Account[];
 }
