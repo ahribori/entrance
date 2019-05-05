131dfeebdfac1ea2c4b96a5eb1c211e869b42431
ahribori <ahribori@gmail.com>
2019-05-06 00:25:57 +0900

    LinkService 구현. (link, unlink)

---
+++ server/src/service/LinkService.ts
@@ -0,0 +1,42 @@
+import uniqueString = require('unique-string');
+import Link from '../db/model/Link';
+
+class LinkService {
+  private static instance: LinkService;
+
+  private constructor() {}
+
+  static getInstance(): LinkService {
+    if (!LinkService.instance) {
+      LinkService.instance = new LinkService();
+    }
+    return LinkService.instance;
+  }
+
+  linkAccount(accountId: number) {
+    return {
+      toApplication: async (applicationId: number) => {
+        return await Link.create({
+          id: uniqueString(),
+          accountId,
+          applicationId,
+        });
+      },
+    };
+  }
+
+  unlinkAccount(accountId: number) {
+    return {
+      fromApplication: async (applicationId: number) => {
+        return await Link.destroy({
+          where: {
+            accountId,
+            applicationId,
+          },
+        });
+      },
+    };
+  }
+}
+
+export default LinkService;
---
+++ server/test/service/LinkService.test.ts
@@ -0,0 +1,48 @@
+import db from '../../src/db';
+import AccountService, { AccountType } from '../../src/service/AccountService';
+import AuthService from '../../src/service/AuthService';
+import ApplicationService from '../../src/service/ApplicationService';
+import LinkService from '../../src/service/LinkService';
+
+const linkService = LinkService.getInstance();
+const accountService = AccountService.getInstance();
+const authService = AuthService.getInstance();
+const applicationService = ApplicationService.getInstance();
+
+let accountId: number;
+let applicationId: number;
+
+beforeAll(async () => {
+  await db.authenticate();
+  const salt = authService.createSalt();
+  const accountCreated = await accountService.createAccount({
+    email: 'link-service-test@gmail.com',
+    username: 'link-service',
+    nickname: '테스트',
+    password: await authService.createPasswordHash('123456', salt),
+    accountType: AccountType.LOCAL,
+    salt,
+  });
+  const applicationCreated = await applicationService.createApplication(
+    '테스트 어플리케이션',
+    accountCreated.id,
+    ['https://ahribori.com'],
+  );
+  accountId = accountCreated.id;
+  applicationId = applicationCreated.id;
+});
+
+describe('LinkService tests', () => {
+  test('Link Account to Application', async () => {
+    const link = await linkService
+      .linkAccount(accountId)
+      .toApplication(applicationId);
+    expect(link).toBeDefined();
+  });
+
+  test('Unlink Account from Application', async () => {
+    const unlink = await linkService
+      .unlinkAccount(accountId)
+      .fromApplication(applicationId);
+  });
+});
