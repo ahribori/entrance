7312cdcf111195ceb4e6ec89183b10bcc2d39735
ahribori <ahribori@gmail.com>
2019-05-07 01:24:48 +0900

    MailService 구현

---
+++ server/src/service/MailService.ts
@@ -0,0 +1,39 @@
+import * as nodemailer from 'nodemailer';
+import config from '../config';
+
+const transportOptions = config.nodeMailer;
+
+const transporter = nodemailer.createTransport({
+  host: transportOptions.host, // hostname
+  secure: true, // use SSL
+  port: transportOptions.port, // port for secure SMTP
+  auth: {
+    user: transportOptions.username,
+    pass: transportOptions.password,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+class MailService {
+  private static instance: MailService;
+  private constructor() {}
+  static getInstance(): MailService {
+    if (!MailService.instance) {
+      MailService.instance = new MailService();
+    }
+    return MailService.instance;
+  }
+
+  async sendMail(mailOptions: {
+    from: string;
+    to: string;
+    subject: string;
+    html: string;
+  }) {
+    return transporter.sendMail(mailOptions);
+  }
+}
+
+export default MailService;
---
+++ server/test/service/MailService.test.ts
@@ -0,0 +1,15 @@
+import MailService from '../../src/service/MailService';
+
+describe('MailService tests', () => {
+  const mailService = MailService.getInstance();
+
+  test('Send mail', async () => {
+    const sendResult = await mailService.sendMail({
+      from: 'Entrance <entrance.auth@gmail.com>',
+      to: 'entrance.auth@gmail.com',
+      subject: '테스트',
+      html: '<p>Hello, world!</p>',
+    });
+    console.log(sendResult)
+  });
+});
