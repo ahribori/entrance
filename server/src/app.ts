9c9f6eeae760eb218f1735197cef0884e7dd4b4f
ahribori <ahribori@gmail.com>
2019-05-07 01:24:13 +0900

    Custom http error handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,18 @@
 import * as express from 'express';
 import api from './api';
 import { config } from './config';
+import { errorHandler } from './middleware/error-handler';
 
 const port: number = config.port;
 const app: express.Application = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/api', api);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
---
+++ server/src/middleware/error-handler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../exception/common/HttpException';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof HttpException) {
+    if (err.status === 500) {
+      return res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+      });
+    }
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  next(err);
+};
