6efbb74683989b6c660d70b50448a704910e8ac7
ahribori <ahribori@gmail.com>
2019-05-09 00:56:01 +0900

    SDK webpack-dev-server 설정

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Entrance sdk')
+enum PropertyName {
+  SDK = 'sdk',
+}
+
+Object.defineProperty(window, 'Entrance', {
+  enumerable: false,
+  configurable: false,
+  writable: false,
+  value: {},
+});
+
+Object.defineProperties((window as any).Entrance, {
+  [PropertyName.SDK]: {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: {},
+  },
+});
--- sdk/webpack.config.js
+++ sdk/webpack.config.js
@@ -1,17 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "sdk.min.js"
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, use: ["ts-loader"], exclude: /node_modules/ }
-        ]
-    }
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'sdk.min.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, use: ['ts-loader'], exclude: /node_modules/ }],
+  },
+  devServer: {
+    contentBase: path.resolve('public'),
+    compress: true,
+    port: 9000,
+  },
 };
