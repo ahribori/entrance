5b8fa0bfde4184bfb4129456e6a1662ab555ced0
ahribori <ahribori@gmail.com>
2019-05-18 18:30:38 +0900

    엑세스토큰을 verify 하는 auth middleware 작성

---
+++ server/src/middleware/auth-middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpException from '../exception/common/HttpException';
+import TokenService from '../service/TokenService';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const accessToken = req.headers.authorization;
+  if (!accessToken) {
+    throw new HttpException(401, '잘못된 토큰입니다.');
+  }
+  try {
+    const verified = TokenService.verifyToken(accessToken);
+    (req as any).accountId = verified.accountId;
+    next();
+  } catch (e) {
+    if (e instanceof JsonWebTokenError) {
+      throw new HttpException(401, '잘못된 토큰입니다.');
+    } else if (e instanceof TokenExpiredError) {
+      throw new HttpException(403, '만료된 토큰입니다.');
+    }
+    throw e;
+  }
+};
+
+export default authMiddleware;
