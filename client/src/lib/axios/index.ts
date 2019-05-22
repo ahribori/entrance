cbe38ed5474664562efb12a32ba231893f178a52
ahribori <ahribori@gmail.com>
2019-05-23 00:07:10 +0900

    axios 인터셉터 설정 -> 500일 때 시스템 메세지 보여주기

--- client/src/lib/axios/index.ts
+++ client/src/lib/axios/index.ts
@@ -1,5 +1,32 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { notification } from 'antd';
 
 const request = axios.create();
+
+request.interceptors.response.use(
+  (response: AxiosResponse) => {
+    // Do something with response data
+    return response;
+  },
+  (error: AxiosError) => {
+    // Do something with response error
+    const { response } = error;
+    if (response) {
+      const { status } = response;
+      switch (status) {
+        case 500: {
+          notification.error({
+            message:
+              '서비스에 문제가 발생했습니다. 잠시 후에 다시 시도해주세요.',
+          });
+          break;
+        }
+        default: {
+        }
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default request;
