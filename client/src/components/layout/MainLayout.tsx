4dcebb0beca27068661d2e67f17e631172ba2f88
ahribori <ahribori@gmail.com>
2019-05-24 00:05:22 +0900

    MainLayout 추가

---
+++ client/src/components/layout/MainLayout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './MainLayout.module.scss';
+
+interface IProps {}
+
+const MainLayout: React.FunctionComponent<IProps> = ({ children }) => {
+  return <div />;
+};
+
+export default MainLayout;
