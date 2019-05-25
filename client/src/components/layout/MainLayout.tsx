4d8299102857ee3d0defada1d872bf9c65dd8abc
ahribori <ahribori@gmail.com>
2019-05-25 17:16:26 +0900

    MainLayout 마크업

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,120 +1,108 @@
 import React from 'react';
 import styles from './MainLayout.module.scss';
-import { Icon, Layout, Menu } from 'antd';
+import { Avatar, Card, Icon, Layout, Menu, PageHeader } from 'antd';
+import { IAccountDetails } from '../../store/AccountStore';
+import Meta from 'antd/es/card/Meta';
+import AuthStore from '../../store/AuthStore';
 
 const { Content, Footer, Sider } = Layout;
-const { SubMenu, ItemGroup } = Menu;
+const { SubMenu } = Menu;
 
-interface IProps {}
+interface IProps {
+  accountDetails: IAccountDetails | null;
+}
 
-const MainLayout: React.FunctionComponent<IProps> = ({ children }) => {
+const SideMenu = () => (
+  <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']}>
+    <SubMenu
+      key="sub1"
+      title={
+        <span>
+          <Icon type="user" />
+          계정정보 관리
+        </span>
+      }
+    >
+      <Menu.Item key="1">내 계정</Menu.Item>
+      <Menu.Item key="2">비밀번호 변경</Menu.Item>
+    </SubMenu>
+    <Menu.Item key="7">
+      <span>
+        <Icon type="laptop" />
+        로그인 이력
+      </span>
+    </Menu.Item>
+    <Menu.Item key="8">
+      <span>
+        <Icon type="deployment-unit" />
+        연결된 서비스 관리
+      </span>
+    </Menu.Item>
+    <Menu.Item key="9">
+      <span>
+        <Icon type="code" />
+        어플리케이션 관리
+      </span>
+    </Menu.Item>
+  </Menu>
+);
+
+const MainLayout: React.FunctionComponent<IProps> = ({
+  children,
+  accountDetails,
+}) => {
+  if (!accountDetails) {
+    return null;
+  }
+
   return (
     <div className={styles.container}>
-      <Menu mode="horizontal">
-        <Menu.Item key="mail">
-          <Icon type="mail" />
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled>
-          <Icon type="appstore" />
-          Navigation Two
-        </Menu.Item>
-        <SubMenu
-          title={
-            <span className="submenu-title-wrapper">
-              <Icon type="setting" />
-              Navigation Three - Submenu
-            </span>
-          }
-        >
-          <ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </ItemGroup>
-          <ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a
-            href="https://ant.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
-      <Layout style={{ padding: '24px 0', background: '#fff' }}>
+      <PageHeader
+        onBack={() => window.history.back()}
+        title="Title"
+        subTitle="This is a subtitle"
+      />
+      <Layout style={{ background: '#fff', borderTop: '1px solid #E8E8E8' }}>
         <Sider
           breakpoint="lg"
           collapsedWidth="0"
-          width={200}
+          width={250}
           style={{ background: '#fff' }}
         >
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%' }}
+          <Card
+            style={{ borderLeft: 'none', borderTop: 'none' }}
+            actions={[
+              <Icon type="setting" />,
+              <Icon
+                type="logout"
+                onClick={() => {
+                  AuthStore.logout();
+                }}
+              />,
+            ]}
           >
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="user" />
-                  subnav 1
-                </span>
+            <Meta
+              avatar={
+                <Avatar icon="user" src={accountDetails.thumbnailImage} />
               }
-            >
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="laptop" />
-                  subnav 2
-                </span>
-              }
-            >
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub3"
-              title={
-                <span>
-                  <Icon type="notification" />
-                  subnav 3
-                </span>
-              }
-            >
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-          </Menu>
+              title={accountDetails.nickname}
+              description={accountDetails.email}
+            />
+          </Card>
+          <SideMenu />
         </Sider>
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+        <Content style={{ padding: '24px', minHeight: 700 }}>
           {children}
         </Content>
       </Layout>
       <Footer
         style={{
           textAlign: 'center',
-          borderTop: '1px solid #dddddd',
-          backgroundColor: '#fff',
+          borderTop: '1px solid #f9f9f9',
+          backgroundColor: '#f9f9f9',
         }}
       >
-        Ant Design ©2018 Created by Ant UED
+        Entrance ©2019 Created by Daniel.hs
       </Footer>
     </div>
   );
